7aa5d908b406d4f23ae5de2eaf511169d4008cf7
WolfGamer12502 <133249173+WolfGamer12502@users.noreply.github.com>
2026-01-24 10:11:15 +0100

    Initialize Flask app and update base HTML template structure

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import pymongo
+import os
+from dotenv import load_dotenv
+
+app = Flask("Focal")
+
+#conection à la bdd
+
+#initialisation de variable...
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0',port=81)
