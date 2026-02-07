a500e289ad6f689dba14e8d06c76bf290f246779
WolfGamer12502 <133249173+WolfGamer12502@users.noreply.github.com>
2026-02-07 10:35:00 +0100

    Add MongoDB connection, update base template, and enhance photo display on index page

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
-import pymongo
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask("Focal")
 
-#conection à la bdd
-
-#initialisation de variable...
-
+mongo = os.getenv('MONGO_URI')
+client = MongoClient(mongo)
+db = client.get_database('Focal')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    photos_data = list(db['photos'].find({}))
+    return render_template("index.html",photos=photos_data)
 
 
 app.run(host='0.0.0.0',port=81)
