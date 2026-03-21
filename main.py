6db84a06b2c787df4929ef4bcc223370c95f43c4
WolfGamer12502 <133249173+WolfGamer12502@users.noreply.github.com>
2026-03-21 09:56:00 +0100

    added the ability to click on article + password encryption

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import os
 from dotenv import load_dotenv
+from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
 app = Flask("Focal")
 app.secret_key = os.getenv('SECRET_KEY', 'change-this-secret-key')  # Nécessaire pour session et flash
+
+bcrypt = Bcrypt(app)
 
 mongo = os.getenv('MONGO_URI')
 client = MongoClient(mongo)
@@ -31,7 +35,8 @@ def signup():
         db_users.insert_one({
             'username': request.form['username'],
             'email':request.form['email'],
-            'password': request.form['password']
+            'password': bcrypt.generate_password_hash(request.form['password']).decode('utf-8'),
+            'role': 'user'
         })
         session['user'] = request.form['username']
         return redirect(url_for('index'))
@@ -46,7 +51,7 @@ def login():
         user = db_users.find_one({'username': request.form['username']})
         if not user:
             return render_template('login.html', erreur="Utilisateur introuvable.")
-        if user['password'] != request.form['password']:
+        if not bcrypt.check_password_hash(user['password'], request.form['password']):
             return render_template('login.html', erreur="Mot de passe incorrect.")
         session['user'] = request.form['username']
         return redirect(url_for('index'))
@@ -63,19 +68,24 @@ def publish():
     if 'user' not in session:
         return render_template('register.html')
     if request.method == "POST":
-        db_articles = db["photos"]
+        db_photos = db["photos"]
         if request.form['title'] and request.form['image']:
-            db_articles.insert_one({
+            db_photos.insert_one({
                 'photo':request.form['image'],
                 'description':request.form['description'],
                 'title':request.form['title'],
                 'user': session['user'],
-                'location_lat': session['location_lat'],
-                'location_long': session['location_long'],
-                'type': session['type'],
+                'location_lat': request.form['location_lat'],
+                'location_long': request.form['location_long'],
+                'type': request.form['type'],
             })
         return redirect(url_for('index'))
     else:
         return render_template('publish.html', erreur = "Fill in all the mandatory fields")
+
+@app.route('/index/<id_photo>')
+def article_open(id_photo):
+    res = db['photos'].find_one({'_id':ObjectId(id_photo)})
+    return render_template('article_open.html', photo = res)
 
 app.run(host='0.0.0.0', port=81)
