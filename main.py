1f9770a55c491d0f74e814229b215c96b4575ef2
WolfGamer12502 <133249173+WolfGamer12502@users.noreply.github.com>
2026-04-11 10:41:25 +0200

    Remove unused import of flash from Flask in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
